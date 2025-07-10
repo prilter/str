c3bbde626fbd09d6c727ed165b204b0cd9b43abe
prilter <bogatyrevm208@gmail.com>
2025-07-10 20:26:29 +0300

    ord_py

--- example.c
+++ example.c
@@ -4,14 +4,6 @@
 int 
 main(void)
 {
-  str s = init_str(AUTO_ALLOC);
-
-  assign_ln(&s, "H e l l o");
-  strip(&s);
-  puts(s.c_str);
-
-  if (free_str(&s))
-    puts("Successful free");
-
+  printf("%d\n", ord(' '));
   return 0;
 }
